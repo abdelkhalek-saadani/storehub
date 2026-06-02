3c6517809f04bfdc1562e0c69cc5bde75042e4a8
Abdelkhalek Saadani <abdelkhaleksaadani@gmail.com>
2026-06-02 14:13:08 +0100

    add responsiveness to review order EP

--- frontend/src/app/component/molecules/review-order/review-order.ts
+++ frontend/src/app/component/molecules/review-order/review-order.ts
@@ -1,6 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { MatExpansionPanel, MatExpansionPanelHeader } from '@angular/material/expansion';
 import { TitleCasePipe } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Breakpoints } from '@core/constants/breakpoints';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-review-order',
@@ -18,16 +21,21 @@ import { TitleCasePipe } from '@angular/common';
         <mat-expansion-panel-header>
           @if (!isOpen()) {
             <div class="pe-4 flex flex-row gap-2">
-              @for (item of items; track item.id) {
+              @for (item of headerItems(); track item.id) {
                 <div class="w-16 rounded-2xl h-16 aspect-square overflow-hidden">
                   <img [src]="item.imgUrl" [alt]="item.name + ' image'" />
+                </div>
+              }
+              @if (headerItems().length != items().length) {
+                <div class="w-16 rounded-2xl h-16 aspect-square flex items-center justify-center">
+                  + {{ items().length - headerItems().length }}
                 </div>
               }
             </div>
           }
         </mat-expansion-panel-header>
         <div class="flex flex-col gap-4">
-          @for (item of items; track item.id) {
+          @for (item of items(); track item.id) {
             <div
               class="p-3 flex flex-row justify-between items-center bg-white rounded-2xl border-b border-b-[#F0EEF0]"
             >
@@ -57,7 +65,16 @@ import { TitleCasePipe } from '@angular/common';
 export class ReviewOrder {
   isOpen = signal(false);
 
-  items = [
+  isXSMobile = signal(false);
+
+  constructor(bpo: BreakpointObserver) {
+    bpo
+      .observe(Breakpoints.sm)
+      .pipe(takeUntilDestroyed())
+      .subscribe((result) => this.isXSMobile.set(result.matches));
+  }
+
+  items = signal([
     {
       id: 1,
       name: 'Natural Milk',
@@ -82,5 +99,44 @@ export class ReviewOrder {
       originalPrice: 6.0,
       price: 5.0,
     },
-  ];
+    {
+      id: 4,
+      name: 'Natural Milk',
+      imgUrl: 'product-img-placeholder.jpg',
+      quantity: 1,
+      originalPrice: 6.0,
+      price: 5.0,
+    },
+    {
+      id: 5,
+      name: 'Natural Milk',
+      imgUrl: 'product-img-placeholder.jpg',
+      quantity: 1,
+      originalPrice: 6.0,
+      price: 5.0,
+    },
+    {
+      id: 6,
+      name: 'Natural Milk',
+      imgUrl: 'product-img-placeholder.jpg',
+      quantity: 1,
+      originalPrice: 6.0,
+      price: 5.0,
+    },
+    {
+      id: 7,
+      name: 'Natural Milk',
+      imgUrl: 'product-img-placeholder.jpg',
+      quantity: 1,
+      originalPrice: 6.0,
+      price: 5.0,
+    },
+  ]);
+
+  headerItems = computed(() => {
+    if (this.isXSMobile()) {
+      return this.items().slice(0, 2);
+    }
+    return this.items().slice(0, 5);
+  });
 }
--- frontend/src/app/core/constants/breakpoints.ts
+++ frontend/src/app/core/constants/breakpoints.ts
@@ -1,3 +1,5 @@
 export const Breakpoints = {
   md: '(min-width: 768px)',
+  sm: '(max-width: 640px)',
+  xs: '(max-width: 435px)',
 } as const;
